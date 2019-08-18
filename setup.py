d7b987dfafb633e4e809db882c7e444481486478
wwj718 <wuwenjie718@gmail.com>
2019-08-18 13:39:44 +0800

    update to 0.9.1

--- setup.py
+++ setup.py
@@ -53,6 +53,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/wwj718/codelab_adapter_client',
-    version='0.9.0',
+    version='0.9.1',
     zip_safe=False,
 )
